8bcc31b5aa62654fb3b5601e4d478f420304b329
Fedor <fedor122448@gmail.com>
2025-01-16 22:01:10 +0400

    Remove redundant import

--- ruler.py
+++ ruler.py
@@ -1,4 +1,3 @@
-import mimetypes
 import os
 import sys
 
@@ -31,41 +30,43 @@ class TreeInfo:
 
     dirs: list[DirInfo] = []
 
+
 def reprint(
     *values: object,
     sep: str | None = " ",
-    end: str | None = "\n",):
-
-    print('\033[1G\033[1A\033[0J', end='')
+    end: str | None = "\n",
+):
+    print("\033[1G\033[1A\033[0J", end="")
     print(*values, sep=sep, end=end)
+
 
 def main():
     if len(sys.argv) < 2:
-        print('Counts lines of code (actually any text files) in a selected directory.')
-        print(f'Usage: python {os.path.basename(__file__)} <path> [exclude]...')
-        print(f'Example: python {os.path.basename(__file__)} {os.sep}{os.path.join("Users", "u", "Code", "some-project")} {os.sep}venv {os.sep}.git {os.sep}.vscode __pycache__')
+        print("Counts lines of code (actually any text files) in a selected directory.")
+        print(f"Usage: python {os.path.basename(__file__)} <path> [exclude]...")
+        print(
+            f"Example: python {os.path.basename(__file__)} {os.sep}{os.path.join('Users', 'u', 'Code', 'some-project')} {os.sep}venv {os.sep}.git {os.sep}.vscode __pycache__"
+        )
         return
     exclude_paths = sys.argv[2:]
     root_path = sys.argv[1]
     tree_info = TreeInfo()
     files_count = 0
-    print('Scanning files...')
-    print('Found: 0')
+    print("Scanning files...")
+    print("Found: 0")
     progress_line_length = 58
     for _, _, files in os.walk(root_path):
         files_count += len(files)
-        reprint(f'Found: {files_count}')
-    print(f'[{" " * progress_line_length}]')
+        reprint(f"Found: {files_count}")
+    print(f"[{' ' * progress_line_length}]")
     files_processed = 0
     for location, _, files in os.walk(root_path):
         files_processed += len(files)
         progress = files_processed * progress_line_length // files_count
-        reprint(f'[{"#" * progress}{" " * (progress_line_length - progress)}]')
+        reprint(f"[{'#' * progress}{' ' * (progress_line_length - progress)}]")
         rel_location = location[len(root_path) :]
         if any(
-            rel_location.startswith(exclude_path)
-            if exclude_path.startswith(os.sep)
-            else exclude_path in rel_location
+            rel_location.startswith(exclude_path) if exclude_path.startswith(os.sep) else exclude_path in rel_location
             for exclude_path in exclude_paths
         ):
             continue
@@ -91,16 +92,16 @@ def main():
             dir_lines_percent = dir_lines * 100 // total_lines
             if dir_lines_percent:
                 dir_info.files.sort(key=lambda f: f.lines, reverse=True)
-                print(f'{dir_info.path if dir_info.path else os.sep}: {dir_lines} ({dir_lines_percent}%)')
+                print(f"{dir_info.path if dir_info.path else os.sep}: {dir_lines} ({dir_lines_percent}%)")
                 for file_info in dir_info.files:
                     file_lines_percent = file_info.lines * 100 // dir_lines
                     if file_lines_percent:
-                        print(f'    {file_info.name}: {str(file_info.lines)} ({file_lines_percent}%)')
+                        print(f"    {file_info.name}: {str(file_info.lines)} ({file_lines_percent}%)")
                 print()
     else:
-        print('No text files found')
-    print(f'Total {total_lines} lines')
+        print("No text files found")
+    print(f"Total {total_lines} lines")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
